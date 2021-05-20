3b0bdc8ed937e4cc3a885b85aaaee4e14d599f7e
Mark <example@gmail.com>
2021-05-20 20:14:08 +0300

    assembling

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+require('es6-promise').polyfill();
+import 'nodelist-foreach-polyfill';
+
 import tabs from './modules/tabs';
 import modal from './modules/modal';
 import timer from './modules/timer';
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@
 let path = require('path');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './js/script.js',
   output: {
     filename: 'bundle.js',
@@ -13,5 +13,22 @@ module.exports = {
 
   devtool: "source-map",
 
-  module: {}
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [['@babel/preset-env', {
+                debug: true,
+                corejs: 3,
+                useBuiltIns: "usage"
+            }]]
+          }
+        }
+      }
+    ]
+  }
 };
